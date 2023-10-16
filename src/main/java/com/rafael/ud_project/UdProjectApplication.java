7d95a07f1a7c911097629301fd996fc2bd69362e
Rafael Domingues <rafael.domingues@confirm8.com>
2023-10-16 20:16:19 -0300

    feat: create initial project

---
+++ src/main/java/com/rafael/ud_project/UdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.ud_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdProjectApplication.class, args);
+	}
+
+}
