b49af75f437cc1276dd83438cea868e06130c75f
Rafael Domingues <rafael.domingues@confirm8.com>
2023-10-16 21:51:23 -0300

    refactor: change category class

--- src/main/java/com/rafael/ud_project/domain/Category.java
+++ src/main/java/com/rafael/ud_project/domain/Category.java
@@ -2,20 +2,68 @@ package com.rafael.ud_project.domain;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@EqualsAndHashCode
+@Entity
 public class Category implements Serializable {
 
-    private Integer id;
+    public static final Long serialVersionUID = 1L; 
 
-    @EqualsAndHashCode.Exclude
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Integer id;
     private String name;
-  
+
+    public Category() {
+
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Category other = (Category) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }  
 }
