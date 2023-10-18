d018d69ab512d9ebaf37c2adbb54c6195dd642e2
Rafael Domingues <rafael.domingues@confirm8.com>
2023-10-18 12:44:21 -0300

    temp: add products mocks to test relationship

--- src/main/java/com/rafael/ud_project/UdProjectApplication.java
+++ src/main/java/com/rafael/ud_project/UdProjectApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafael.ud_project.domain.Category;
+import com.rafael.ud_project.domain.Product;
 import com.rafael.ud_project.repositories.CategoryRepository;
+import com.rafael.ud_project.repositories.ProductRepository;
 
 @SpringBootApplication
 public class UdProjectApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoryRepository repository;
+	private CategoryRepository catRepository;
+	@Autowired
+	private ProductRepository prodRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdProjectApplication.class, args);
@@ -25,7 +29,20 @@ public class UdProjectApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(null, "Games");
 		Category cat2 = new Category(null, "Books");
-		this.repository.saveAll(Arrays.asList(cat1, cat2));
+		
+		Product prod1 = new Product(null, "PlaysStation 5", 5000.00);
+		Product prod2 = new Product(null, "Xbox Series S", 3000.00);
+		Product prod3 = new Product(null, "Livro Clean Code", 100.00);
+
+		cat1.getProducts().addAll(Arrays.asList(prod1, prod2));
+		cat2.getProducts().addAll(Arrays.asList(prod3));
+
+		prod1.getCategories().addAll(Arrays.asList(cat1));
+		prod2.getCategories().addAll(Arrays.asList(cat1));
+		prod3.getCategories().addAll(Arrays.asList(cat2));
+
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		prodRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 
 }
